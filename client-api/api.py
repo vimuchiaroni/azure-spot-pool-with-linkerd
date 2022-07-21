3244f461c5270b59d39a88a66ad6aea3c2eeb948
victor.muchiaroni <victor.muchiaroni@brmalls.com.br>
2022-07-21 12:08:04 -0300

    Initial Commit

---
+++ client-api/api.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask import make_response
+from os import environ
+import requests as r 
+
+
+app_name = 'client-api'
+app = Flask(app_name)
+app.debug = True
+cart_service_url=environ['CART_SERVICE_URL']
+
+@app.route('/create/cart', methods=['POST'])
+def create_cart():
+    request_data = request.get_json()
+    
+    _return = r.post(f'{cart_service_url}/api/create/cart', json=request_data)
+    
+    if 200 == _return.status_code:
+    
+
+        response = {
+                'status': 'SUCCESS',
+                'message': _return.content,
+                }
+        statusCode = 200
+
+    else:
+        response = {
+                'status': 'FAILED',
+                'message': _return.content,
+                }
+        statusCode = 500
+
+    return make_response(_return.content, statusCode)
+
+@app.route('/get/carts')
+def get_carts():
+
+    _return = r.get(f'{cart_service_url}/api/carts')
+    return make_response(_return.content, _return.status_code)
+
+@app.route('/retries/upstream/cart/<retries_quantity>')
+def retries_upstream_cart(retries_quantity):
+    _return = r.get(f'{cart_service_url}/api/retries/{retries_quantity}')
+    return make_response(_return.content, _return.status_code)
